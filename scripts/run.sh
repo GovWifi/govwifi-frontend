f29e27086fa7643414e510757d28c9c61d5bca03
Karl Royer <karl@bigmight.com>
2026-06-11 15:22:16 +0100

    New govlogger args added

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,15 +8,20 @@ if [ "x${GOVLOGGER_FILE}" == "x" ]; then
     GOVLOGGER_FILE="/healthcheck/govlogs.log"
 fi
 
+if [ "x${ROTATED_GOVLOGGER_FILE}" == "x" ]; then
+    ROTATED_GOVLOGGER_FILE="/healthcheck/govlogs.rotated"
+fi
+
 if [ "x${GOVLOGGER_LOG_PROG_COMMAND}" == "x" ]; then
-    GOVLOGGER_LOG_PROG_COMMAND="/usr/bin/process_gov_logs --pretty --canonical --state /healthcheck/statefile --expires 30 --reduce_duplicates --reduce_freeradius_proxied --reduce_last_date_only --reduce_drop_eap_peap --wait 6 --logfile ${GOVLOGGER_FILE} >> /healthcheck/reduced_logs.out 2>>/healthcheck/process_gov_logs.err"
+    GOVLOGGER_LOG_PROG_COMMAND="/usr/bin/process_gov_logs --pretty --delete --canonical --state /healthcheck/statefile --expires 30 --reduce_duplicates --reduce_freeradius_proxied --reduce_last_date_only --reduce_drop_eap_peap --logfile ${ROTATED_GOVLOGGER_FILE} >> /healthcheck/reduced_logs.out 2>>/healthcheck/process_gov_logs.err"
 fi
 
 export GOVLOGGER_FILE
+export ROTATED_GOVLOGGER_FILE
 export GOVLOGGER_LOG_PROG_COMMAND
 
 c_rehash /etc/raddb/certs/trusted_certificates
 
+freeradius_exporter -web.listen-address 0.0.0.0:9812 &
 cd /healthcheck && bundle exec puma -p 3000 &
-/usr/local/sbin/radiusd ${RADIUSD_PARAMS} &
-freeradius_exporter -web.listen-address 0.0.0.0:9812
+exec /usr/local/sbin/radiusd ${RADIUSD_PARAMS}
